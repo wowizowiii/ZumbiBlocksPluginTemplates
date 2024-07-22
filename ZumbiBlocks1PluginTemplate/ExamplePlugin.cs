172d2ec21c2d8f09158749e71d726667321cbaf1
nyawoi <106277673+nyawoi@users.noreply.github.com>
2024-07-22 15:40:16 -0500

    Add ZB1 template

---
+++ ZumbiBlocks1PluginTemplate/ExamplePlugin.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ExamplePlugin;
+
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+public class ExamplePlugin : BaseUnityPlugin
+{
+    public const string PluginGUID = PluginAuthor + "." + PluginName;
+    public const string PluginAuthor = "AuthorName";
+    public const string PluginName = "ExamplePlugin";
+    public const string PluginVersion = "0.1.0";
+
+    internal new static ManualLogSource Logger;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGUID);
+    }
+}
---
+++ ZumbiBlocks1PluginTemplate/Patches/sc_gamecontrolPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ExamplePlugin;
+
+[HarmonyPatch(typeof(sc_gamecontrol))]
+public static class sc_gamecontrolPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(sc_gamecontrol.Start))]
+    public static void RetrieveLanguagePack(sc_gamecontrol __instance)
+    {
+        ExamplePlugin.Logger.LogInfo($"Current game version is {__instance.buildversion}");
+    }
+}
