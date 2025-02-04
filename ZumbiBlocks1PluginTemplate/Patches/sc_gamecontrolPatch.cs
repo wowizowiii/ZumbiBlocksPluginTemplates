5f003d62a08171a7ff47d4ef6958e9bf862f67fd
nyawoi <106277673+nyawoi@users.noreply.github.com>
2025-02-04 16:06:25 -0600

    Correctly name ExamplePatch

--- ZumbiBlocks1PluginTemplate/Patches/sc_gamecontrolPatch.cs
+++ ZumbiBlocks1PluginTemplate/Patches/sc_gamecontrolPatch.cs
@@ -7,7 +7,7 @@ public static class sc_gamecontrolPatch
 {
     [HarmonyPostfix]
     [HarmonyPatch(nameof(sc_gamecontrol.Start))]
-    public static void RetrieveLanguagePack(sc_gamecontrol __instance)
+    public static void ExamplePatch(sc_gamecontrol __instance)
     {
         ExamplePlugin.Logger.LogInfo($"Current game version is {__instance.buildversion}");
     }
